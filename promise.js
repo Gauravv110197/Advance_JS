b172be75d3ec78a752be46cc3451abc5958f3585
Gaurav Singh <imriitikk7@gmail.com>
2026-08-24 10:07:35 +0530

    update promise

--- promise.js
+++ promise.js
@@ -1,15 +1,15 @@
-// Promise((resolve, reject) => {  })
-// const promise = new Promise((resolve, reject) => { } 
-// ) = > {
-//     success = true ;
-//     if(success) {
-//     resolve();
-//     }
-//     else {
-//         reject();
-//     }
+// // Promise((resolve, reject) => {  })
+// // const promise = new Promise((resolve, reject) => { } 
+// // ) = > {
+// //     success = true ;
+// //     if(success) {
+// //     resolve();
+// //     }
+// //     else {
+// //         reject();
+// //     }
 
-// }
+// // }
 const promise1 = new Promise((resolve, reject) => {
   const success = true;
 
@@ -23,13 +23,13 @@ const promise1 = new Promise((resolve, reject) => {
   }
 });
 
-promise1
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// promise1
+//   .then((response) => {
+//     console.log(response);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
 
 
 const promise2 = new Promise((resolve, reject) => {
@@ -45,29 +45,73 @@ const promise2 = new Promise((resolve, reject) => {
   }
 });
 
-promise2
-  .then((message) => {
-    console.log(message);
-  })
-  .catch((error) => {
+// // promise2
+// //   .then((message) => {
+// //     console.log(message);
+// //   })
+// //   .catch((error) => {
+// //     console.log(error.message);
+// //     });
+
+
+
+// Promise.all([promise1, promise2])
+//     .then((response) => {
+//         console.log(response);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     });    
+
+
+// Promise.allSettled([promise1, promise2])
+//     .then((response) => {
+//         console.log(response);      
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     });
+
+promise1
+.then((response) => {
+return response
+})
+.then((response) => {
+    console.log(response.username);
+})
+.catch((error) => {
     console.log(error.message);
+});
+
+
+
+Promise.any([promise1, promise2])
+.then((response) => {
+    console.log(response);
+})
+.catch((error) => {
+    console.log(error.message);
+});
+
+function fetchUserData() {
+    return new Promise((resolve, reject) => {
+        let success = true;
+        if (success) {
+            resolve({   
+                id: 123,
+                username: "Gaurav"
+            });
+        } else {
+            reject(new Error("Failed to fetch user data"));
+        }
     });
+}
 
 
 
-Promise.all([promise1, promise2])
-    .then((response) => {
-        console.log(response);
-    })
-    .catch((error) => {
-        console.log(error);
-    });    
+async function getUser() {
+    const user = await fetchUserData();
+    console.log(user);
+}
 
-
-Promise.race([promise1, promise2])
-    .then((response) => {
-        console.log(response);      
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+getUser()
