c781a4e1d94156450fcb1c61e2ef7a6b23b433da
Gaurav Singh <imriitikk7@gmail.com>
2026-08-24 09:43:35 +0530

    promise

---
+++ promise.js
@@ -0,0 +1,73 @@
+// Promise((resolve, reject) => {  })
+// const promise = new Promise((resolve, reject) => { } 
+// ) = > {
+//     success = true ;
+//     if(success) {
+//     resolve();
+//     }
+//     else {
+//         reject();
+//     }
+
+// }
+const promise1 = new Promise((resolve, reject) => {
+  const success = true;
+
+  if (success) {
+    resolve({
+      id: 1111,
+      username: "Gaurav"
+    });
+  } else {
+    reject(new Error("Data not fetched"));
+  }
+});
+
+promise1
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+
+const promise2 = new Promise((resolve, reject) => {
+  const success = true;
+
+ if (success) {
+    resolve({
+      id: 2222,
+      username: "Singh"
+    });
+  } else {
+    reject(new Error("Data not fetched"));
+  }
+});
+
+promise2
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    });
+
+
+
+Promise.all([promise1, promise2])
+    .then((response) => {
+        console.log(response);
+    })
+    .catch((error) => {
+        console.log(error);
+    });    
+
+
+Promise.race([promise1, promise2])
+    .then((response) => {
+        console.log(response);      
+    })
+    .catch((error) => {
+        console.log(error);
+    });
