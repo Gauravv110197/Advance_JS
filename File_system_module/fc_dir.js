5208ba62e59df9fac1502ff93dcbd2b917e05b99
Gaurav Singh <imriitikk7@gmail.com>
2026-08-31 10:05:02 +0530

    fc_dir

---
+++ File_system_module/fc_dir.js
@@ -0,0 +1,10 @@
+const fs = require("fs")
+
+fs.mkdir("./myFolder", (err) => {
+    if(err){
+        console.log(err);
+        return;
+
+    }
+    console.log("Folder created successfully");
+});
