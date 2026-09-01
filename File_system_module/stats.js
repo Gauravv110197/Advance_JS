60dddd87b50590ea15d4b6c08c3d46d29df29b36
Gaurav Singh <imriitikk7@gmail.com>
2026-09-01 12:41:46 +0530

    myfolder1

--- File_system_module/stats.js
+++ File_system_module/stats.js
@@ -5,17 +5,29 @@ fs.stat("notes.txt",(err,stats)=>{
         console.log(err);
         return;
     }
-    console.log("Information about[notes.txt]",stats);
-    console.log("Size of the file:", stats.size);
-    console.log("Creation time of the file:", stats.birthtime.toISOString(  ).split("T"));
-    console.log("Last modified time of the file:", stats.mtime.toISOString().split("T"));
+    // console.log("Information about[notes.txt]",stats);
+    // console.log("Size of the file:", stats.size);
+    // console.log("Creation time of the file:", stats.birthtime.toISOString(  ).split("T"));
+    // console.log("Last modified time of the file:", stats.mtime.toISOString().split("T"));
 
 })
 
-const user = {
-    name: "Gaurav",
-    age: 90,
-    city: "GHZ"
+// const user = {
+//     name: "Gaurav",
+//     age: 90,
+//     city: "GHZ"
 
-}
-user["name"] 
+// }
+// user["name"] 
+
+
+
+
+
+fs.stat("./myFolder1",(err,stats)=>{
+    if(err) {
+        console.log(err);
+        return;
+    }
+    console.log(stats.size);
+});
