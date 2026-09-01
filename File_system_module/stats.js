aaf7ec14eccf15948019fa90c655e9a5ce3bb0c4
Gaurav Singh <imriitikk7@gmail.com>
2026-09-01 12:12:36 +0530

    stats.js

---
+++ File_system_module/stats.js
@@ -0,0 +1,20 @@
+const fs = require("fs")
+
+fs.stat("notes.txt",(err,stats)=>{
+    if(err) {
+        console.log(err);
+        return;
+    }
+    console.log("Information about[notes.txt]",stats);
+    console.log("Size of the file:", stats.size);
+    console.log("Creation time of the file:", stats.birthtime.toISOString(  ).split("T"));
+
+})
+
+const user = {
+    name: "Gaurav",
+    age: 90,
+    city: "GHZ"
+
+}
+user["name"] 
