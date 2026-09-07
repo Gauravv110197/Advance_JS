f78a7b16dac981143a94c35d7c48e074226d0cd4
Gaurav Singh <imriitikk7@gmail.com>
2026-09-07 10:20:58 +0530

    read and write streams

---
+++ File_system_module/Streams/app.js
@@ -0,0 +1,35 @@
+import fs from "fs";    
+
+//Readable Streams
+
+const readStream = fs.createReadStream("input.txt",
+    {encoding : "utf-8"}
+)
+
+
+readStream.on("data", (chunk) => {
+    console.log("Data Received ");
+    console.log("Data :", chunk)
+})
+
+readStream.on("end", () => {
+    console.log("END");
+})
+
+
+readStream.on("error", (error) => {
+    console.log("Error :", error.message);
+})
+
+//Create Write Stream
+const writeStream = fs.createWriteStream("output.txt");
+
+writeStream.write("yeahhhh\n");
+
+writeStream.on("finish", () => {
+    console.log("Data has been written");
+})
+
+writeStream.on("error", (error) => {   
+    console.log("Error :", error.message);
+})
