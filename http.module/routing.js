a02bc9d5b2195bc9382dca418b65a4a8d67ef3cb
Gaurav Singh <imriitikk7@gmail.com>
2026-09-15 12:54:40 +0530

    routing home,about,contact

--- http.module/routing.js
+++ http.module/routing.js
@@ -1,11 +1,32 @@
 import http from 'http'
+import fs from 'fs'
 
 const server = http.createServer((req, res) => {
     console.log('hello world')
     console.log(req.url)
-    res.end('Hello from the server')
+
+    if (req.url === "/") {
+
+        const data = fs.readFileSync("home.html", "utf8")
+        res.end(data)
+
+    } else if (req.url === "/about") {
+
+        const data = fs.readFileSync("about.html", "utf8")
+        res.end(data)
+
+    } else if (req.url === "/contact") {
+
+        const data = fs.readFileSync("contact.html", "utf8")
+        res.end(data)
+
+    } else {
+
+        res.end("404 Page Not Found")
+
+    }
 })
 
-server.listen(3001, "127.0.0.1", () => {
-    console.log("Server is running on port 3001")
+server.listen(3002, "127.0.0.1", () => {
+    console.log("Server is running on port 3002")
 })
